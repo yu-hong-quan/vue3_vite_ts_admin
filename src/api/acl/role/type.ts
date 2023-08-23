05f5e364a07bf4cc646b509624276c8c1213dc76
xiaoyu <15017872695@163.com>
2023-08-23 23:10:57 +0800

    feta: 角色管理功能开发

--- src/api/acl/role/type.ts
+++ src/api/acl/role/type.ts
@@ -1,35 +1,35 @@
 // 用户角色管理数据类型
 export interface ResponseData {
-    code: number,
-    message: string,
-    ok: boolean,
+  code: number;
+  message: string;
+  ok: boolean;
 }
 export interface RoleData {
-    id: number | string,
-    createTime: string
-    updateTime: string,
-    roleName: string,
-    remark: string
+  id: number | string;
+  createTime: string;
+  updateTime: string;
+  roleName: string;
+  remark: string;
 }
 
 export interface ReadonlyRoleData extends Partial<RoleData> {
-    // 特殊参数必填区
+  // 特殊参数必填区
 }
 
-export type Records = ReadonlyRoleData[]
+export type Records = ReadonlyRoleData[];
 
 export interface RoleResponseData extends ResponseData {
-    data: {
-        records: Records,
-        total: number,
-        size: number,
-        current: number,
-        orders: [],
-        countId: null,
-        maxLimit: null,
-        pages: number
-        optimizeCountSql: boolean,
-        hitCount: boolean,
-        searchCount: boolean,
-    }
+  data: {
+    records: Records;
+    total: number;
+    size: number;
+    current: number;
+    orders: [];
+    countId: null;
+    maxLimit: null;
+    pages: number;
+    optimizeCountSql: boolean;
+    hitCount: boolean;
+    searchCount: boolean;
+  };
 }
--- src/views/acl/role/index.vue
+++ src/views/acl/role/index.vue
@@ -6,31 +6,89 @@
           <el-input placeholder="请输入关键字搜索" v-model="keyword"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="search" :disabled="keyword ? false : true">搜索</el-button>
+          <el-button
+            type="primary"
+            @click="search"
+            :disabled="keyword ? false : true"
+          >
+            搜索
+          </el-button>
           <el-button type="primary" @click="reset">重置</el-button>
         </el-form-item>
       </el-form>
     </el-card>
-    <el-card style="margin-top: 20px;">
-      <el-button type="primary" icon="Plus" @click="addRole">添加角色</el-button>
-      <el-table border :data="allRole" v-loading="listLoadding" style="margin:10px 0;">
-        <el-table-column type="index" align="center" label="序号" width="60"></el-table-column>
-        <el-table-column label="ID" align="center" prop="id" width="100"></el-table-column>
-        <el-table-column label="角色名称" align="center" prop="roleName" show-overflow-tooltip></el-table-column>
-        <el-table-column label="创建时间" align="center" prop="createTime" show-overflow-tooltip></el-table-column>
-        <el-table-column label="更新时间" align="center" prop="updateTime" show-overflow-tooltip></el-table-column>
+    <el-card style="margin-top: 20px">
+      <el-button type="primary" icon="Plus" @click="addRole">
+        添加角色
+      </el-button>
+      <el-table
+        border
+        :data="allRole"
+        v-loading="listLoadding"
+        style="margin: 10px 0"
+      >
+        <el-table-column
+          type="index"
+          align="center"
+          label="序号"
+          width="60"
+        ></el-table-column>
+        <el-table-column
+          label="ID"
+          align="center"
+          prop="id"
+          width="100"
+        ></el-table-column>
+        <el-table-column
+          label="角色名称"
+          align="center"
+          prop="roleName"
+          show-overflow-tooltip
+        ></el-table-column>
+        <el-table-column
+          label="创建时间"
+          align="center"
+          prop="createTime"
+          show-overflow-tooltip
+        ></el-table-column>
+        <el-table-column
+          label="更新时间"
+          align="center"
+          prop="updateTime"
+          show-overflow-tooltip
+        ></el-table-column>
         <el-table-column label="操作" width="260" align="center">
           <template #="{ row }">
-            <el-button type="primary" size="small" icon="User" @click="setRole(row)">
+            <el-button
+              type="primary"
+              size="small"
+              icon="User"
+              @click="setRole(row)"
+            >
               分配角色
             </el-button>
-            <el-button type="primary" size="small" icon="Edit" @click="ediltRole(row)">
+            <el-button
+              type="primary"
+              size="small"
+              icon="Edit"
+              @click="ediltRole(row)"
+            >
               编辑角色
             </el-button>
-            <el-popconfirm title="您确定要删除该角色吗?" confirm-button-text="确定" cancel-button-text="取消" @confirm="removeUser(row)"
-              :width="220">
+            <el-popconfirm
+              title="您确定要删除该角色吗?"
+              confirm-button-text="确定"
+              cancel-button-text="取消"
+              @confirm="removeUser(row)"
+              :width="220"
+            >
               <template #reference>
-                <el-button type="danger" size="small" icon="Delete" style="margin-top: 5px">
+                <el-button
+                  type="danger"
+                  size="small"
+                  icon="Delete"
+                  style="margin-top: 5px"
+                >
                   删除用户
                 </el-button>
               </template>
@@ -38,22 +96,35 @@
           </template>
         </el-table-column>
       </el-table>
-      <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[10, 20, 50, 100]"
-        :background="true" layout="prev, pager, next, jumper,->,sizes, total," :total="total" @current-change="getHasRole"
-        @size-change="sizeChange" />
+      <el-pagination
+        v-model:current-page="pageNo"
+        v-model:page-size="pageSize"
+        :page-sizes="[10, 20, 50, 100]"
+        :background="true"
+        layout="prev, pager, next, jumper,->,sizes, total,"
+        :total="total"
+        @current-change="getHasRole"
+        @size-change="sizeChange"
+      />
     </el-card>
 
-    <el-dialog v-model="dialogFormVisible" :title="roleParams.id ? '更新角色' : '添加角色'" width="50%">
+    <el-dialog
+      v-model="dialogFormVisible"
+      :title="roleParams.id ? '更新角色' : '添加角色'"
+      width="50%"
+    >
       <el-form :model="roleParams" :rules="rules" ref="form">
         <el-form-item label="角色名称" prop="roleName">
-          <el-input v-model="roleParams.roleName" placeholder="请输入角色名称" autocomplete="off" />
+          <el-input
+            v-model="roleParams.roleName"
+            placeholder="请输入角色名称"
+            autocomplete="off"
+          />
         </el-form-item>
       </el-form>
       <template #footer>
         <el-button @click="dialogFormVisible = false">取消</el-button>
-        <el-button type="primary" @click="save">
-          确定
-        </el-button>
+        <el-button type="primary" @click="save">确定</el-button>
       </template>
     </el-dialog>
   </div>
@@ -62,8 +133,16 @@
 <script lang="ts" setup>
 import { ref, reactive, onMounted, nextTick } from 'vue';
 import useLayOutSettingStore from '@/store/modules/setting';
-import { reqAllRoleList, reqAddRoleOrUpdateRole, reqRemoveRole } from '@/api/acl/role/index';
-import type { Records, RoleResponseData, ReadonlyRoleData } from '@/api/acl/role/type'
+import {
+  reqAllRoleList,
+  reqAddRoleOrUpdateRole,
+  reqRemoveRole,
+} from '@/api/acl/role/index';
+import type {
+  Records,
+  RoleResponseData,
+  ReadonlyRoleData,
+} from '@/api/acl/role/type';
 import { ElMessage } from 'element-plus';
 
 let settingStore = useLayOutSettingStore();
@@ -76,92 +155,97 @@ let allRole = ref<Records>([]);
 let dialogFormVisible = ref<boolean>(false);
 let form = ref<any>(null);
 let roleParams = reactive<ReadonlyRoleData>({
-  roleName: ''
-})
+  roleName: '',
+});
 const ValidityRoleName = (rule: any, value: string, callBack: any) => {
   console.log(rule);
 
   if (value.trim().length >= 2) {
-    callBack()
+    callBack();
   } else {
-    callBack(new Error('角色名称至少两位'))
+    callBack(new Error('角色名称至少两位'));
   }
-}
+};
 const rules = {
-  roleName: [
-    { required: true, trigger: 'blur', validator: ValidityRoleName }
-  ]
-}
+  roleName: [{ required: true, trigger: 'blur', validator: ValidityRoleName }],
+};
 
 const getHasRole = async (pager = 1) => {
   listLoadding.value = true;
   pageNo.value = pager;
-  let result: RoleResponseData = await reqAllRoleList(pageNo.value, pageSize.value, keyword.value);
+  let result: RoleResponseData = await reqAllRoleList(
+    pageNo.value,
+    pageSize.value,
+    keyword.value,
+  );
   if (result.code === 200) {
     listLoadding.value = false;
     allRole.value = result.data.records;
     total.value = result.data.total;
   }
-}
+};
 
 const sizeChange = () => {
-  getHasRole()
-}
+  getHasRole();
+};
 
 const search = () => {
-  getHasRole()
-  keyword.value = "";
-}
+  getHasRole();
+  keyword.value = '';
+};
 
 const reset = () => {
-  settingStore.refsh = !settingStore.refsh
-}
+  settingStore.refsh = !settingStore.refsh;
+};
 
 const addRole = () => {
   dialogFormVisible.value = true;
   Object.assign(roleParams, {
     roleName: '',
-    id: 0
-  })
+    id: 0,
+  });
   nextTick(() => {
     form.value.clearValidate('roleName');
-  })
-}
+  });
+};
 
 const ediltRole = (row: ReadonlyRoleData) => {
   Object.assign(roleParams, row);
   dialogFormVisible.value = true;
   nextTick(() => {
     form.value.clearValidate('roleName');
-  })
-}
+  });
+};
 
 const save = async () => {
   await form.value.validate();
   let result: any = await reqAddRoleOrUpdateRole(roleParams);
   if (result.code === 200) {
-    ElMessage({ type: 'success', message: roleParams.id ? '更新成功' : '添加成功' })
+    ElMessage({
+      type: 'success',
+      message: roleParams.id ? '更新成功' : '添加成功',
+    });
     dialogFormVisible.value = false;
-    getHasRole(roleParams.id ? pageNo.value : 1)
+    getHasRole(roleParams.id ? pageNo.value : 1);
   }
-}
+};
 
 const removeUser = async (row: ReadonlyRoleData) => {
-  let result: any = await reqRemoveRole((row.id as number));
+  let result: any = await reqRemoveRole(row.id as number);
   if (result.code === 200) {
-    ElMessage({ type: 'success', message: '删除成功' })
-    getHasRole(pageNo.value)
+    ElMessage({ type: 'success', message: '删除成功' });
+    getHasRole(pageNo.value);
   } else {
-    ElMessage({ type: 'error', message: '删除失败' })
+    ElMessage({ type: 'error', message: '删除失败' });
   }
-}
+};
 
 const setRole = (row: ReadonlyRoleData) => {
   console.log(row);
-}
+};
 
 onMounted(() => {
-  getHasRole()
+  getHasRole();
 });
 
 defineOptions({
